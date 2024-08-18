f311d680a97ba0b3acdc551f7bce4eff007790bc
tildejustin <spacepepper3.14@gmail.com>
2024-08-18 01:13:41 -0400

    upgradle: 2024 everything, srapi and wp

---
+++ src/main/java/me/duncanruns/icarus/IcarusConfig.java
@@ -0,0 +1,14 @@
+package me.duncanruns.icarus;
+
+import org.mcsr.speedrunapi.config.api.SpeedrunConfig;
+import org.mcsr.speedrunapi.config.api.annotations.Config;
+
+public class IcarusConfig implements SpeedrunConfig {
+    @Config.Numbers.Whole.Bounds(min = 1, max = 3)
+    public static int flightDuration = 3;
+
+    @Override
+    public String modID() {
+        return "icarus";
+    }
+}
